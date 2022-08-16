55b0702831bb0e4673628e71f56e0de49c150b59
RajputNandini <109655286+nandinirajput@users.noreply.github.com>
2022-08-16 21:29:07 +0530

    Add files via upload

---
+++ strings_methods_part2.py
@@ -0,0 +1,24 @@
+# print("hello".zfill(8))#000hello. it fill the missing characters with zeros .from left side.
+# print("hello".zfill(3))#if we give less number it shows the same how many characters are there it will show tha
+
+# print("hello sir".centre(13,"$"))#$$hello sir$$.this is use to fill the special characters or by default it 
+# print("hellosir".centre(15,"%"))#%%%%hellosir%%%
+
+# v="  "
+# u=" "
+# w=""
+# x="    jjcl bb"
+# print(v.isspace())  #true. isspace is gives true when it has single space or multiple space.
+# print(u.isspace())  #true. it is true coz it has single space.
+# print(w.isspace())  #false.isspace is gives false when it has empty string and some thing written in the string
+# print(x.isspace())  #false. it gives false coz something is written in string so it gives fals.
+# print("".isspace()) #false
+# print(" ".isspace())#true
+
+# print("hello nandini".index("1")) #2.this is used to check where the variable is lying.the index always starts
+# print("hello challo".index("o",6)) #11.if we have multiple letters then this way is used.
+# print("one plus".index("p")) #4
+# print("khello jahello hello".index("e",3)) #10
+# print("khelo jahello hello".index("e",11)) #16
+
+# print("hello pythoonhy".rindex("o")) #11. the rindex which means right side index it counts from right side 
